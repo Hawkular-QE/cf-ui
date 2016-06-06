e78d4b07fbcfd1718b89be7c37d58b5070b0cc47
mattmahoneyrh <mmahoney@redhat.com>
2016-06-06 15:03:06 -0400

    Fleshed out Servers Details

--- tests/test_servers.py
+++ tests/test_servers.py
@@ -15,6 +15,13 @@ def web_session(request):
     return web_session
 
 def test_servers_view(web_session):
+    web_session.logger.info("Begin Server View")
     servs = servers(web_session)
     assert (servs.validate_servers_list())
+
+def test_server_details(web_session):
+    web_session.logger.info("Begin Server Details")
+    ui_pairs = servers(web_session).validate_server_details()
+    assert ui_pairs["Product"] == web_session.PROVIDER
+    assert ui_pairs["Middleware Provider"] == web_session.HAWKULAR_PROVIDER_NAME
 
--- views/servers.py
+++ views/servers.py
@@ -1,16 +1,95 @@
 from common.ui_utils import ui_utils
 
+
 class servers():
     web_session = None
+    web_driver = None
     ui_utils = None
 
+    PROVIDER = "Hawkular"
+    
     def __init__(self, web_session):
         self.web_session = web_session
+        self.web_driver = web_session.web_driver
         self.ui_utils = ui_utils(self.web_session)
 
     def validate_servers_list(self):
+
+        ## Vaidate that a Hawkular Provider is in the Servers list
+        ## To-Do: Validate Servers list contains all expected Servers
+
         ## Refactor when formal navigations are in place
         self.web_session.web_driver.get("{}/middleware_server/show_list".format(self.web_session.MIQ_URL))
 
-        ## Refactore when table directory is in place
-        return self.ui_utils.isTextOnPage(self.web_session.HAWKULAR_PROVIDER_NAME)
+        # 1) Get table as list
+        # 2) Create row of cell-elements
+        # 3) Search each row to validate whether "Hawkular" provider is present
+
+        table = []
+        table = self.__get_table_list__()
+
+        for row in table:
+            # Search the each item in the row, for Product "Hawkular"
+            for value in row:
+                if value == self.web_session.PROVIDER:
+                    self.web_session.logger.info("Found {} Provider".format(self.web_session.PROVIDER))
+                    return True
+
+        self.web_session.logger.info("No {} Provider found".format(self.web_session.PROVIDER))
+        return False
+
+
+    def validate_server_details(self):
+
+        ## Validate the Hawkular Server details
+
+        ## Refactor when formal navigations are in place
+        self.web_session.web_driver.get("{}/middleware_server/show_list?type=list".format(self.web_session.MIQ_URL))
+
+        self.__find_and_click_on_provider__(self.web_session.PROVIDER)
+
+        # 1) Get table as list
+        # 2) Convert to Dictionary
+        # 3) Return Dictionary
+
+        table = []
+        table = self.__get_table_list__()
+
+        ## Convert to Dictionary
+        pairs = {}
+        for pair in table:
+            pairs[pair[0]] = pair[1]
+
+        return pairs
+
+    def __getTable_list__(self):
+        return self.web_driver.find_elements_by_xpath('.//tr')
+
+
+    def __get_table_list__(self):
+        table = []
+        for tr in self.__getTable_list__():
+            tds = tr.find_elements_by_tag_name('td')
+            if tds:
+                table.append([td.text for td in tds])  # Create the row/list
+
+        return table
+
+    def __find_and_click_on_provider__(self, product_name):
+
+        ## Click on first row that is found to contain Product-name
+        ## To-Do: how to handle multiple providers with same Product-name
+
+        table = []
+        for tr in self.__getTable_list__():
+            tds = tr.find_elements_by_tag_name('td')
+            if tds:
+                table.append([td.text for td in tds])
+                for row in table:
+                    for value in row:
+                        if value == product_name:
+                            self.web_session.logger.info("Click on {} Provider".format(product_name))
+                            tds[3].click()
+                            return;
+
+        assert False, "Did not find {} Provider".format(product_name)
