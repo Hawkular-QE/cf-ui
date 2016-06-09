14b54ba6dc3cf8aa5a771fce6aa3bfc0464b5ea3
Valerii Sorokin <vsorokin@redhat.com>
2016-06-09 16:23:22 +0200

    Accommodated new change in navigation

--- navigation/navigation.py
+++ navigation/navigation.py
@@ -65,26 +65,27 @@ class UI_Route():
 class NavigationTree():
 
     """ Reflection of CF UI, structure and content """
-    _tree = dict([])
+    _tree = dict([]) # class attribute, common for all instances
+    __is_tree_built = False
     web_driver = None
     _pivot = None
 
     def add_point(self, name, location, action):
         self._tree.update({ name : UI_Action( UI_Point(name, location), UI_Operation(action)) } )
 
-
     def __init__(self, driver):
 
         self._pivot = driver
         self.web_driver = driver
-        self.add_point(   "compute", ".//*[@id='maintab']/li[3]/a/span[2]", "Hover")
-        self.add_point("middleware", "id('#menu-compute')/ul/li[4]/a/span", "Hover")
-
-        self.add_point("middleware_providers",   "id('#menu-compute')/ul/li[4]/div/ul/li[1]/a/span", "Click")
-        self.add_point("middleware_servers",     "id('#menu-compute')/ul/li[4]/div/ul/li[2]/a/span", "Click")
-        self.add_point("middleware_deployments", "id('#menu-compute')/ul/li[4]/div/ul/li[3]/a/span", "Click")
-        self.add_point("middleware_datasources", "id('#menu-compute')/ul/li[4]/div/ul/li[4]/a/span", "Click")
-        self.add_point(              "topology", "id('#menu-compute')/ul/li[4]/div/ul/li[5]/a/span", "Click")
+        if self.__is_tree_built == False:
+            self.add_point("middleware", "id('maintab')/li[6]/a/span[2]", "Hover")
+            self.add_point("middleware_providers",   "id('#menu-mdl')/ul/li[1]/a/span", "Click")
+            self.add_point("middleware_servers",     "id('#menu-mdl')/ul/li[2]/a/span", "Click")
+            self.add_point("middleware_deployments", "id('#menu-mdl')/ul/li[3]/a/span", "Click")
+            self.add_point("middleware_datasources", "id('#menu-mdl')/ul/li[4]/a/span", "Click")
+            self.add_point(              "topology", "id('#menu-mdl')/ul/li[5]/a/span", "Click")
+            print "\nTree is built."
+            self.__is_tree_built = True
 
     def navigate(self, route):
         driver = self.web_driver
@@ -102,17 +103,27 @@ class NavigationTree():
 
 
     def navigate_to_middleware_providers_view(self):
-        self.navigate(UI_Route("compute").add("middleware").add("middleware_providers"))
+        #self.navigate(UI_Route("compute").add("middleware").add("middleware_providers"))
+        self.navigate( UI_Route("middleware").add("middleware_providers") )
+        print "\nExecution: navigate_to_middleware_providers_view"
 
     def navigate_to_middleware_servers_view(self):
-        self.navigate(UI_Route("compute").add("middleware").add("middleware_servers"))
+        #self.navigate(UI_Route("compute").add("middleware").add("middleware_servers"))
+        self.navigate( UI_Route("middleware").add("middleware_servers") )
+        print "\nExecution: navigate_to_middleware_servers_view"
 
     def navigate_to_middleware_deployment_view(self):
-        self.navigate(UI_Route("compute").add("middleware").add("middleware_deployments"))
+        #self.navigate(UI_Route("compute").add("middleware").add("middleware_deployments"))
+        self.navigate( UI_Route("middleware").add("middleware_deployments") )
+        print "\nExecution: navigate_to_middleware_deployment_view"
 
     def navigate_to_middleware_datasources_view(self):
-        self.navigate(UI_Route("compute").add("middleware").add("middleware_datasources"))
+        #self.navigate(UI_Route("compute").add("middleware").add("middleware_datasources"))
+        self.navigate( UI_Route("middleware").add("middleware_datasources") )
+        print "\nExecution: navigate_to_middleware_datasources_view"
 
     def navigate_to_topology_view(self):
-        self.navigate(UI_Route("compute").add("middleware").add("topology"))
+        #self.navigate(UI_Route("compute").add("middleware").add("topology"))
+        self.navigate( UI_Route("middleware").add("topology") )
+        print "\nExecution: navigate_to_topology_view"
 
