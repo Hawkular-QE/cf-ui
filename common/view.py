89e21c8bd573822580c7851482dc6dabbd217cd5
Prachi Yadav <pyadav@pyadav.pnq.csb>
2016-06-09 23:37:42 +0530

    Edited Grid, Tile, List view test case

--- common/view.py
+++ common/view.py
@@ -17,27 +17,17 @@ class view():
     web_session = None
 
     def __init__(self,web_session):
-
         self.web_session = web_session
-
         self.web_driver = web_session.web_driver
 
-
-
-
-    def gridView(self):
-
+    def grid_View(self):
         grid = self.web_driver.find_element_by_name("view_grid")
         grid.click()
 
-
-    def tileView(self):
-
-
+    def tile_View(self):
         tile = self.web_driver.find_element_by_name("view_tile")
         tile.click()
 
-    def listView(self):
-
+    def list_View(self):
         list = self.web_driver.find_element_by_name("view_list")
         list.click()
--- tests/test_view.py
+++ tests/test_view.py
@@ -1,6 +1,5 @@
 import pytest
 from common.session import session
-from views.servers import servers
 from common.view import view
 from navigation.navigation import NavigationTree
 '''
@@ -18,16 +17,14 @@ def web_session(request):
     web_session = session()
     return web_session
 
-
 def test_view(web_session):
-
     driver = web_session.web_driver
     nav = NavigationTree(driver)
     nav.navigate_to_middleware_deployment_view()
     viewHaw = view(web_session)
     print ("Show Grid View")
-    viewHaw.gridView()
+    viewHaw.grid_View()
     print ("Show Tile View")
-    viewHaw.tileView()
+    viewHaw.tile_View()
     print ("Show List View")
-    viewHaw.listView()
+    viewHaw.list_View()
