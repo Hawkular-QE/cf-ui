f5759e8330053b07c622acbc769554e29d04a29a
Valerii Sorokin <vsorokin@redhat.com>
2016-06-10 14:01:15 +0200

    Corrected output format of test methods

--- parsing/table.py
+++ parsing/table.py
@@ -1,25 +1,27 @@
 
 class Table():
 
-    keys = [ 'middleware_servers' ,         'middleware_deployments' ,          'middleware_datasources',           'middleware_providers'     ]
-    urls = [ '/middleware_server/show_list','/middleware_deployment/show_list', '/middleware_datasource/show_list', '/ems_middleware/show_list' ]
+    paths = {
+                'middleware_servers'     : '/middleware_server/show_list',
+                'middleware_deployments' : '/middleware_deployment/show_list',
+                'middleware_datasources' : '/middleware_datasource/show_list',
+                'middleware_providers'   :  '/ems_middleware/show_list'
+            }
 
 
     def __init__(self, web_session):
         self.web_session = web_session
         self.driver = web_session.web_driver
-        self.paths = {key: value for key, value in zip(self.keys, self.urls)}
 
     def from_url(self, url):
         self.driver.get(self.web_session.MIQ_HOSTNAME + url)
         return self
 
-
     def elements(self):
         list = self.driver.find_element_by_name("view_list")
         list.click()
 
-        dictionary = dict()
+        dictionary = []
         headers = []
         theads = self.driver.find_elements_by_xpath('.//table//thead')
         table_rows = self.driver.find_elements_by_xpath('.//table//tbody//tr')
@@ -36,32 +38,26 @@ class Table():
 
                 head_iter = iter(headers[0])
                 table_row_list = []
-                dict_rows = []
+                cell = dict()
                 for td in table_row_point:
 
-                    dict_row = {head_iter.next() : td.text}
-                    table_row_list.append(dict_row)
-                    dict_rows.append(table_row_list)
+                    head_next = head_iter.next()
+                    if head_next != '':
+                        cell.update({ head_next : td.text })
+                        table_row_list = cell
 
-            dictionary.update({ "row_{}".format(row_number) : table_row_list})
-
-        full_table = dict({"metadata": []})
-        full_table.update({"row_array" : dictionary})
-
-        return full_table
+            dictionary.append( table_row_list)
+        return dictionary
 
 
     def get_middleware_providers_table(self):
         return self.from_url( self.paths.get('middleware_providers')).elements()
 
-
     def get_middleware_servers_table(self):
         return self.from_url(self.paths.get('middleware_servers')).elements()
 
-
-    def get_middleware_w_deployments_table(self):
+    def get_middleware_deployments_table(self):
         return self.from_url(self.paths.get('middleware_deployments')).elements()
-
 
     def get_middleware_datasources_table(self):
         return self.from_url(self.paths.get('middleware_datasources')).elements()
--- tests/test_parsing_table.py
+++ tests/test_parsing_table.py
@@ -15,11 +15,30 @@ def web_session(request):
     return web_session
 
 
-def test_sucess(web_session):
-    print 'Start parsing'
+def test_1_instance(web_session):
+
     table = Table(web_session)
 
-    print table.get_middleware_datasources_table()
-    print table.get_middleware_providers_table()
-    print table.get_middleware_servers_table()
-    print table.get_middleware_w_deployments_table()
+    print "List of middleware datasources: ", table.get_middleware_datasources_table(), "\n"
+    print "List of middleware providers: ", table.get_middleware_providers_table(), "\n"
+    print "List of middleware deployments: ", table.get_middleware_deployments_table(), "\n"
+
+    servers_list = table.get_middleware_servers_table()
+    print "Full servers list: ", servers_list, "\n"
+
+    """
+
+    # In case of 2 present entities:
+        print "Feed 1: ", servers_list[0]["Feed"]
+        print "Feed 2: ", servers_list[1]["Feed"]
+
+    """
+
+
+def test_multi_instances(web_session):
+
+    print "List of middleware datasources: ", Table(web_session).get_middleware_datasources_table(), "\n"
+    print "List of middleware providers: ",   Table(web_session).get_middleware_providers_table(), "\n"
+    print "List of middleware deployments: ", Table(web_session).get_middleware_deployments_table(), "\n"
+    print "Middleware servers list: ",        Table(web_session).get_middleware_servers_table(), "\n"
+
