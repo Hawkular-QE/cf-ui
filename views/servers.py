038d271396a6143ef1facd6800fccff8f601f255
mattmahoneyrh <mmahoney@redhat.com>
2016-06-10 12:42:18 -0400

    Refactored to use table class

--- views/servers.py
+++ views/servers.py
@@ -1,12 +1,11 @@
 from common.ui_utils import ui_utils
+from parsing.table import table
 from navigation.navigation import NavigationTree
 
 class servers():
     web_session = None
     web_driver = None
     ui_utils = None
-
-    PROVIDER = "Hawkular"
 
     def __init__(self, web_session):
         self.web_session = web_session
@@ -15,26 +14,18 @@ class servers():
 
     def validate_servers_list(self):
 
-        ## Vaidate that a Hawkular Provider is in the Servers list
-        ## To-Do: Validate Servers list contains all expected Servers
+        servers_list = table(self.web_session).get_middleware_servers_table()
 
-        NavigationTree(self.web_driver).navigate_to_middleware_servers_view()
+        # Search each row to validate whether "Hawkular" provider is present
+        # To-Do: Validate all servers, once there is a way to get the expected servers list
 
-        # 1) Get table as list
-        # 2) Create row of cell-elements
-        # 3) Search each row to validate whether "Hawkular" provider is present
+        for server in servers_list:
+             if server.get('Product') == self.web_session.PROVIDER:
+                self.web_session.logger.info("Found {} Provider".format(self.web_session.PROVIDER))
+                return True
 
-        table = []
-        table = self.__get_table_list__()
+        self.web_session.logger.error("No {} Provider found".format(self.web_session.PROVIDER))
 
-        for row in table:
-            # Search the each item in the row, for Product "Hawkular"
-            for value in row:
-                if value == self.web_session.PROVIDER:
-                    self.web_session.logger.info("Found {} Provider".format(self.web_session.PROVIDER))
-                    return True
-
-        self.web_session.logger.info("No {} Provider found".format(self.web_session.PROVIDER))
         return False
 
 
