8216494e0d0c00fd3278d43210a09e4fe382e39b
mattmahoneyrh <mmahoney@redhat.com>
2016-06-13 13:56:16 -0400

    Moved test.test.py to framework

---
+++ tests/framework/test_test.py
@@ -0,0 +1,20 @@
+import pytest
+from common.session import session
+
+
+@pytest.fixture
+def web_session(request):
+    web_session = session(add_provider=False)
+
+    def closeSession():
+        web_session.logger.info("Close browser session")
+        web_session.close_web_driver()
+    request.addfinalizer(closeSession)
+
+    return web_session
+
+def test_test(web_session):
+    web_session.logger.info("Begin Test")
+    assert True, "Sanity Login Test"
+
+
