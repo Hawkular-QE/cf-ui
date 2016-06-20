965a013f7d7b1563e69a26d77b4ca3cbf997cc3e
Valerii Sorokin <vsorokin@redhat.com>
2016-06-20 17:03:21 +0200

    Clearly splitted branches

--- tests/framework/test_parsing_table.py
+++ tests/framework/test_parsing_table.py
@@ -15,30 +15,38 @@ def web_session(request):
     return web_session
 
 
-def test_1_instance(web_session):
+def _test_instance(web_session):
 
     t = table(web_session)
 
-    print "List of middleware datasources: ", t.get_middleware_datasources_table(), "\n"
+    print "List of middleware datasources: ", t.get_middleware_datasource_table(), "\n"
     print "List of middleware providers: ", t.get_middleware_providers_table(), "\n"
     print "List of middleware deployments: ", t.get_middleware_deployments_table(), "\n"
 
     servers_list = t.get_middleware_servers_table()
     print "Full servers list: ", servers_list, "\n"
 
-    """
 
-    # In case of 2 present entities:
-        print "Feed 1: ", servers_list[0]["Feed"]
-        print "Feed 2: ", servers_list[1]["Feed"]
+def test_details(web_session):
 
-    """
+    t = table(web_session)
+
+    datasources_table = t.get_middleware_datasource_table()
+    ds_num = len(datasources_table)
+    print "List of middleware datasources ({}): ".format(ds_num)
+    t.pretty_print(datasources_table)
+
+    print "\nList of middleware datasources: "
+    t.pretty_print( t.get_datasource_details() )
+
+    print "List of middleware providers: "
+    t.pretty_print( t.get_providers_details())
+
+    print "List of middleware deployments: "
+    t.pretty_print( t.get_deployments_details())
+
+    print "Middleware servers list: "
+    t.pretty_print( t.get_servers_details())
 
 
-def test_multi_instances(web_session):
-
-    print "List of middleware datasources: ", table(web_session).get_middleware_datasources_table(), "\n"
-    print "List of middleware providers: ",   table(web_session).get_middleware_providers_table(), "\n"
-    print "List of middleware deployments: ", table(web_session).get_middleware_deployments_table(), "\n"
-    print "Middleware servers list: ",        table(web_session).get_middleware_servers_table(), "\n"
 
