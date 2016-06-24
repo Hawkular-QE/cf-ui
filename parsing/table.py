fd883702688152de1fda8927b8f6b42f3e7d918f
Valerii Sorokin <vsorokin@redhat.com>
2016-06-24 13:06:18 +0200

    Direct details page fetch

--- parsing/table.py
+++ parsing/table.py
@@ -13,6 +13,7 @@ class table():
         self.web_session = web_session
         self.driver = web_session.web_driver
 
+    # temporary while fast navigation is merged
     def is_url_different(self, url):
         return (self.driver.current_url != url)
 
@@ -25,6 +26,26 @@ class table():
     def from_url(self, url):
         if self.is_url_different(url):
             self.driver.get(url)
+        return self
+
+    ## TODO: utils for usage in navigation (move to navigation.py)
+
+    def is_url(self):
+        pass
+    def is_path(self):
+        pass
+
+    def build_subtree(self, target):
+        """
+        if target.is_url:
+            self.from_url(target)
+        else:
+            if target.is_path():
+                self.from_path(target)
+        else:
+            print "Wrong target!"
+        """
+        self.from_path(target)
         return self
 
     def elements(self):
@@ -63,6 +84,7 @@ class table():
     def details(self):
         table = []
         dict = {}
+
         for tr in self.driver.find_elements_by_xpath('.//tbody/tr'):
             tds = tr.find_elements_by_tag_name('td')
             if tds:
@@ -73,13 +95,11 @@ class table():
                 self.web_session.logger.warning("No element found for table.")
         return dict
 
-    def get_leaf(self, page, number):
-        leaf = []
-        req_path = self.paths.get(page)
-        leaf_url = req_path.replace("/show_list", "/show/")
-        leaf = self.from_url(leaf_url).details()
+    def get_leaf(self, number):
+
+        leaf = self.details()
         dict_size = len(leaf)
-        leaf.update({'web_page': leaf_url})
+        leaf.update({'web_page': self.driver.current_url})
         leaf.update({'Length': dict_size})
         if dict_size == 0:
             leaf.update({'Message': 'Unexpected error encountered'})
@@ -89,20 +109,10 @@ class table():
         details = []
         req_path  = self.paths.get(page)
 
-        urls = self.from_path(req_path).get_leafes_urls()
+        urls = self.from_path(req_path).get_leaves_urls()
         for url in urls:
-            #details.append(self.get_leaf(req_path, n ))
-
-            table = self.from_url(url).details()
-
-            # additional metadata
-            dict_size = len(table)
-            page = self.driver.current_url
-            table.update({'web_page' : page})
-            table.update({'Length' : dict_size })
-            if dict_size == 0:
-                table.update({'Message' : 'Unexpected error encountered'})
-            details.append(table)
+            self.from_url(url)
+            details.append(self.details())
         return details
 
 
@@ -122,45 +132,59 @@ class table():
         return self.from_path(self.paths.get('middleware_datasources')).elements()
 
 
-    def get_leafes_urls(self):
+    def get_leaves_refs(self):
         from lxml import etree
         d = etree.HTML(self.driver.page_source)
         xpath_selector = ".//tbody/tr/td/input[@class='list-grid-checkbox']/@value"
         values = d.xpath(xpath_selector)
+        return values
+
+
+    def get_leaves_urls(self):
         base_url = self.driver.current_url.replace("/show_list", "/show/")
-        urls = [ base_url+value for value in values]
+        urls = [ base_url+value for value in self.get_leaves_refs()]
         return urls
 
-    """
-    def get_leaf(self, url):
-        return self.from_url(url).details()
 
+    def get_page_details_ref(self, page, ref):
+        base_path = self.paths.get(page)
+        sub_path = base_path.replace("/show_list", "/show/")
+        target = "{}/{}".format(sub_path, ref)
+        return self.build_subtree(target).get_leaf(ref)
 
-    def get_all(self):
-        all = []
-        for url in self.get_leafes_urls():
-            all.append( self.get_leaf(url) )
-        return all
-    """
 
     def get_datasource_details(self):
         return self.get_page_details('middleware_datasources')
 
-    def get_datasource_details_of(self, offset):
-        return self.get_page_details('middleware_datasources')
+    def get_datasource_details_ref(self, ref):
+        return self.get_page_details_ref('middleware_datasource', ref)
+
 
     def get_servers_details(self):
         return self.get_page_details('middleware_servers')
+
+    def get_servers_details_ref(self, ref):
+        return self.get_page_details_ref('middleware_servers', ref)
 
 
     def get_providers_details(self):
         return self.get_page_details('middleware_providers')
 
+    def get_providers_detail_ref(self, ref):
+        return self.get_page_details_ref('middleware_providers', ref)
+
 
     def get_deployments_details(self):
         return self.get_page_details('middleware_deployments')
 
+    def get_deployments_details_ref(self, ref):
+        return self.get_page_details_ref('middleware_deployments', ref)
 
-    def pretty_print(self, complex_data):
+    ## TODO: next - to use python's magics with method's names substitution for eliminate this spagetti! -^^
+
+
+
+    def pretty_print(self, complex_data, caption="DUMP: "):
         import pprint as pp
+        print caption
         pp.pprint(complex_data)
