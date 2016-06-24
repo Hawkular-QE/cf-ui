abcb35b0528910f786944698d8002e55b4384377
mattmahoneyrh <mmahoney@redhat.com>
2016-06-24 14:40:02 -0400

    Added asserts and logging

--- views/deployments.py
+++ views/deployments.py
@@ -15,20 +15,28 @@ class deployments():
 
     def validate_deployment_details(self):
         deployments_ui = table(self.web_session).get_middleware_deployments_table()
+        assert deployments_ui, "No UI Deployments found."
         deployments_hawk = self.hawkular_api.get_hawkular_deployments()
+        assert deployments_hawk, "No Hawkular Deployments found."
 
         # Test only a random sameple of the entire Deployments list
         for dep in self.ui_utils.get_random_list(deployments_ui, 3):
             deployment_name = dep.get('Deployment Name')
+            self.web_session.logger.info("Validate Deployment {}.".format(deployment_name))
 
             self.web_session.web_driver.get("{}/middleware_deployment/show_list".format(self.web_session.MIQ_URL))
+            assert self.ui_utils.waitForTextOnPage("Middleware Deployments", 15)
 
             self.ui_utils.click_on_row_containing_text(deployment_name)
-            self.ui_utils.waitForTextOnPage("Properties", 15)
+            self.ui_utils.waitForTextOnPage("Nativeid", 15)
             dep_details_ui = self.ui_utils.get_generic_table_as_dict()
+            assert dep_details_ui, "UI Deployment Details not found for {}.".format(deployment_name)
+            self.web_session.logger.error("dep_details_ui: {}".format(dep_details_ui))
+
 
             dep_details_hawk = self.ui_utils.find_row_in_list(deployments_hawk, 'Name', deployment_name)
             assert dep_details_hawk, "Hawkular Deployment {} not found.".format(deployment_name)
+            self.web_session.logger.error("dep_details_hawk: {}".format(dep_details_hawk))
 
             assert dep_details_ui.get('Middleware Provider') == self.web_session.HAWKULAR_PROVIDER_NAME
             assert dep_details_ui.get('Name') == dep_details_hawk.get('Name')
