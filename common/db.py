643321f47436f63df4c7c88fee79d68bd2ac5b58
mattmahoneyrh <mmahoney@redhat.com>
2016-07-01 12:12:33 -0400

    Added DB class

---
+++ common/db.py
@@ -0,0 +1,56 @@
+import psycopg2
+import psycopg2.extras
+
+class db():
+    web_session = None
+    miq_ip = None
+    db_port = None
+    username = None
+    password = None
+
+    database = 'vmdb_production'
+    connection = None
+    dict_cursor = None
+
+    # To-Do: return only needed columns (aka add filters)
+    sql_servers = 'select * from middleware_servers'
+    sql_datasources = 'select * from middleware_datasources'
+    sql_deployments = 'select * from middleware_deployments'
+
+    def __init__(self, web_session, miq_ip = None, username = None, password = None, db_port = None):
+
+        self.web_session = web_session
+        self.miq_ip = self.web_session.MIQ_HOSTNAME if db_port == None else miq_ip
+        self.username = self.web_session.DB_USERNAME if username == None else username
+        self.password = self.web_session.DB_PASSWORD if password == None else password
+        self.db_port = self.web_session.DB_PORT if db_port == None else db_port
+
+        try:
+            self.connection = psycopg2.connect(
+                database=self.database, user=self.username, password=self.password, host=self.miq_ip, port=self.db_port)
+            self.dict_cursor = self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        except Exception, e:
+            raise Exception(e)
+
+
+    def execute(self, query):
+        rows = None
+
+        self.web_session.logger.info('Execute SQL \"{}\" on IP \"{}\".'.format(query, self.miq_ip))
+
+        try:
+            self.dict_cursor.execute(query)
+            rows = self.dict_cursor.fetchall()
+        except Exception, e:
+            self.web_session.logger.error('Failed to execute SQL \"{}\". Exception: {}".'.format(query, e))
+
+        return rows
+
+    def get_servers(self):
+        return self.execute(self.sql_servers)
+
+    def get_datasources(self):
+        return self.execute(self.sql_datasources)
+
+    def get_deployments(self):
+        return self.execute(self.sql_deployments)
---
+++ tests/framework/test_db.py
@@ -0,0 +1,32 @@
+import pytest
+from common.session import session
+from common.db import db
+
+@pytest.fixture (scope='session')
+def web_session(request):
+    web_session = session(add_provider=False, login=False)
+
+    def closeSession():
+        web_session.logger.info("Close browser session")
+        web_session.close_web_driver()
+    request.addfinalizer(closeSession)
+
+    return web_session
+
+def test_db_datasources(web_session):
+    rows = db(web_session).get_datasources()
+    assert rows
+    for row in rows:
+        print "name: ", row.get('name')
+
+def test_db_servers(web_session):
+    rows = db(web_session).get_servers()
+    assert rows
+    for row in rows:
+        print "product: ", row.get('product')
+
+def test_db_deployments(web_session):
+    rows = db(web_session).get_deployments()
+    assert rows
+    for row in rows:
+        print "name: ", row.get('name')
