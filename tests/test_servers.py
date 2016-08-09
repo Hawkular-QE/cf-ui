f48691da59a319ef1d528c201b8ff4b21f944565
Sunil Kondkar <skondkar@dhcp201-106.englab.pnq.redhat.com>
2016-08-09 16:01:46 +0530

    Added test for redeployment of application archive.

--- tests/test_servers.py
+++ tests/test_servers.py
@@ -64,3 +64,6 @@ def test_cfui_deploy_application_archive(web_session):
 
 def test_cfui_undeploy_application_archive(web_session):
     assert servers(web_session).undeploy_application_archive()
+
+def test_cfui_redeploy_application_archive(web_session):
+    assert servers(web_session).redeploy_application_archive()
--- views/servers.py
+++ views/servers.py
@@ -354,11 +354,22 @@ class servers():
 
         return True
 
-    def redeploy_application_archive(self):
-        # Find EAP with application to redeploy
-        # Redeploy
-        # Validate that application is still in list
+    def redeploy_application_archive(self, app_to_redeploy=APPLICATION_WAR):
 
+        # Find EAP with application to redeploy
+        NavigationTree(self.web_session).navigate_to_middleware_deployment_view()
+
+        deployments_ui = table(self.web_session).get_middleware_deployments_table()
+        self.ui_utils.click_on_row_containing_text(app_to_redeploy)
+
+        # Redeploy
+
+        self.redeploy_server_deployment(app_to_redeploy)
+
+        # Validate that application status is enabled:
+        # ( Existing issues: https://github.com/ManageIQ/manageiq/issues/9876, Issue#10138 )
+
+        assert self.ui_utils.refresh_until_text_appears('Enabled', 300)
         return True
 
     def wait_for_eap_state(self, feed, expected_state, wait_time):
@@ -427,6 +438,15 @@ class servers():
         self.ui_utils.sleep(2)
         self.web_driver.switch_to_alert().accept()
         self.ui_utils.waitForTextOnPage('Undeployment initiated for selected deployment(s)', 15)
+
+    def redeploy_server_deployment(self, app_to_redeploy=APPLICATION_WAR):
+        self.web_session.logger.info("Redeploying App: {}".format(app_to_redeploy))
+        self.web_driver.find_element_by_id('middleware_deployment_deploy_choice').click()
+        self.web_driver.find_element_by_id(
+            'middleware_deployment_deploy_choice__middleware_deployment_redeploy').click()
+        self.ui_utils.sleep(2)
+        self.web_driver.switch_to_alert().accept()
+        self.ui_utils.waitForTextOnPage('Redeployment initiated for selected deployment(s)', 15)
 
     def wait_for_deployment_state(self, deployment_name, state, wait_time):
         currentTime = time.time()
