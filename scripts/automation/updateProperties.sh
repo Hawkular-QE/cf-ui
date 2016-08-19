1fc5f658ea43a00c6d63b70e67eff311ebe09484
mattmahoneyrh <mmahoney@redhat.com>
2016-08-19 11:13:54 -0400

    Added RECORD_TESTS Check

--- scripts/automation/updateProperties.sh
+++ scripts/automation/updateProperties.sh
@@ -4,4 +4,12 @@ sed -i "/HAWKULAR_HOSTNAME /c\HAWKULAR_HOSTNAME = $HAWKULAR_HOSTNAME" conf/prope
 sed -i "/MIQ_HOSTNAME /c\MIQ_HOSTNAME = $MIQ_HOSTNAME" conf/properties.properties
 sed -i "/HAWKULAR_PORT /c\HAWKULAR_PORT = $HAWKULAR_PORT" conf/properties.properties
 sed -i "/DISPLAY_PORT /c\DISPLAY_PORT = $DISPLAY_PORT" conf/properties.properties
-sed -i "/RECORD_TESTS /c\RECORD_TESTS = True" conf/properties.properties
+
+if [[ -z $RECORD_TESTS ]]
+then
+    RECORD_TESTS="True"
+fi
+
+echo "Setting RECORD_TESTS to: $RECORD_TESTS"
+sed -i "/RECORD_TESTS /c\RECORD_TESTS = $RECORD_TESTS" conf/properties.properties
+
