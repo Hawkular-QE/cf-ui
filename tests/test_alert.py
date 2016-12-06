acb63aaa8e1e1fa2a592360868a9cf5fe5da6a67
Prachi Yadav <pyadav@pyadav.pnq.csb>
2016-12-06 19:54:45 +0530

    logging added

--- tests/test_alert.py
+++ tests/test_alert.py
@@ -5,6 +5,13 @@ from views.eap_alerts import eap_alerts
 @pytest.fixture (scope='session')
 def web_session(request):
     web_session = session(add_provider=True)
+    web_session.logger.info("ALR-001: Define Alerts based upon EAP metrics")
+
+    def closeSession():
+        web_session.logger.info("Close browser session")
+        web_session.close_web_driver()
+
+    request.addfinalizer(closeSession)
 
     return web_session
 
