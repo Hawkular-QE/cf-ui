d436d87413c8a9878aa5eba9e624e59b91510376
Prachi Yadav <pyadav@pyadav.pnq.csb>
2017-01-02 15:56:11 +0530

    edited Test and added kill EAP Test

--- tests/test_eap_operations.py
+++ tests/test_eap_operations.py
@@ -12,7 +12,9 @@ def web_session(request):
         web_session.close_web_driver()
 
     request.addfinalizer(closeSession)
+
     return web_session
+
 
 def test_eap_stop(web_session):
     web_session.logger.info("Begin eap Stop Test")
@@ -22,19 +24,31 @@ def test_eap_restart(web_session):
     web_session.logger.info("Begin eap Restart Test")
     assert eap_operations(web_session).restart_eap()
 
-def test_eap_gracefull_stop(web_session):
-    web_session.logger.info("Begin eap Gracefull Stop Test")
-    assert eap_operations(web_session).gracefull_stop_eap()
+def test_eap_kill(web_session):
+    web_session.logger.info("Begin eap Kill Test")
+    assert eap_operations(web_session).kill_eap()
 
-def test_eap_reload(web_session):
-    web_session.logger.info("Begin eap Reload Test")
-    assert eap_operations(web_session).reload_eap()
+def test_eap_start(web_session):
+    web_session.logger.info("Begin eap Start Test")
+    assert eap_operations(web_session).start_eap()
 
 def test_eap_suspend(web_session):
     web_session.logger.info("Begin eap Suspend Test")
     assert eap_operations(web_session).suspend_eap()
 
 def test_eap_resume(web_session):
-    web_session.logger.info("Begin eap Reume Test")
+    web_session.logger.info("Begin eap Resume Test")
     assert eap_operations(web_session).resume_eap()
+
+def test_eap_reload(web_session):
+    web_session.logger.info("Begin eap Reload Test")
+    assert eap_operations(web_session).reload_eap()
+
+
+
+
+
+
+
+
 
--- views/eap_operations.py
+++ views/eap_operations.py
@@ -21,97 +21,53 @@ class eap_operations():
         self.web_session = web_session
         self.web_driver = web_session.web_driver
 
-    def stop_eap(self):
+    def eap_power(self):
         self.web_session.web_driver.get("{}/middleware_domain/show_list".format(self.web_session.MIQ_URL))
-        assert ui_utils(self.web_session).waitForTextOnPage("Domain Name ",15)
+        assert ui_utils(self.web_session).waitForTextOnPage("Middleware Domains", 15)
         self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'master')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("master (Summary)",15)
+        assert ui_utils(self.web_session).waitForTextOnPage("master (Summary)", 15)
         self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Server Groups')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage(" Server Group Name ", 15)
+        assert ui_utils(self.web_session).waitForTextOnPage("Server Group Name", 15)
         self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'main-server-group')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Properties ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Servers')]")
+        assert ui_utils(self.web_session).waitForTextOnPage("main-server-group (Summary)", 15)
+        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Servers')]").click()
         assert ui_utils(self.web_session).waitForTextOnPage("Server Name", 15)
         self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'server-one')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Properties",15)
+        assert ui_utils(self.web_session).waitForTextOnPage("Properties", 15)
         self.web_session.web_driver.find_element_by_xpath("//button[@title='Power']").click()
-        self.web_session.web_driver.find_element_by_xpath("//a[@title='Stop this server']") .click()
+
+
+
+    def stop_eap(self):
+        self.eap_power()
+        self.web_session.web_driver.find_element_by_xpath("//a[@title='Stop this server']").click()
         ui_utils(self.web_session).accept_alert(20)
         assert ui_utils(self.web_session).waitForTextOnPage("Stop initiated for selected server(s)", 30)
         return True
 
     def restart_eap(self):
-        self.web_session.web_driver.get("{}/middleware_domain/show_list".format(self.web_session.MIQ_URL))
-        assert ui_utils(self.web_session).waitForTextOnPage("Domain Name ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'master')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("master (Summary)", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Server Groups')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage(" Server Group Name ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'main-server-group')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Properties ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Servers')]")
-        assert ui_utils(self.web_session).waitForTextOnPage("Server Name", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'JBoss EAP')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Properties", 15)
-        self.web_session.web_driver.find_element_by_xpath("//button[@title='Power']").click()
+        self.eap_power()
         self.web_session.web_driver.find_element_by_xpath("//a[@title='Restart this server']").click()
         ui_utils(self.web_session).accept_alert(20)
         assert ui_utils(self.web_session).waitForTextOnPage("Restart initiated for selected server(s)", 15)
         return True
 
-    def gracefull_stop_eap(self):
-        self.web_session.web_driver.get("{}/middleware_domain/show_list".format(self.web_session.MIQ_URL))
-        assert ui_utils(self.web_session).waitForTextOnPage("Domain Name ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'master')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("master (Summary)", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Server Groups')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage(" Server Group Name ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'main-server-group')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Properties ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Servers')]")
-        assert ui_utils(self.web_session).waitForTextOnPage("Server Name", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'JBoss EAP')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Properties", 15)
-        self.web_session.web_driver.find_element_by_xpath("//button[@title='Power']").click()
+    def kill_eap(self):
+        self.eap_power()
         self.web_session.web_driver.find_element_by_xpath("//a[@title='Kill this server']").click()
         ui_utils(self.web_session).accept_alert(20)
-        assert ui_utils(self.web_session).waitForTextOnPage("Shutdown initiated for selected server(s)", 15)
+        assert ui_utils(self.web_session).waitForTextOnPage("Kill initiated for selected server(s)", 15)
         return True
 
     def reload_eap(self):
-        self.web_session.web_driver.get("{}/middleware_domain/show_list".format(self.web_session.MIQ_URL))
-        assert ui_utils(self.web_session).waitForTextOnPage("Domain Name ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'master')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("master (Summary)", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Server Groups')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage(" Server Group Name ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'main-server-group')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Properties ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Servers')]")
-        assert ui_utils(self.web_session).waitForTextOnPage("Server Name", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'JBoss EAP')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Properties", 15)
-        self.web_session.web_driver.find_element_by_xpath("//button[@title='Power']").click()
+        self.eap_power()
         self.web_session.web_driver.find_element_by_xpath("//a[contains(@id,'reload')]").click()
         ui_utils(self.web_session).accept_alert(20)
         assert ui_utils(self.web_session).waitForTextOnPage("Reload initiated for selected server(s)", 15)
-
         return True
 
     def suspend_eap(self):
-        self.web_session.web_driver.get("{}/middleware_domain/show_list".format(self.web_session.MIQ_URL))
-        assert ui_utils(self.web_session).waitForTextOnPage("Domain Name ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'master')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("master (Summary)", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Server Groups')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage(" Server Group Name ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'main-server-group')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Properties ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Servers')]")
-        assert ui_utils(self.web_session).waitForTextOnPage("Server Name", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'JBoss EAP')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Properties", 15)
-        self.web_session.web_driver.find_element_by_xpath("//button[@title='Power']").click()
+        self.eap_power()
         self.web_session.web_driver.find_element_by_xpath("//a[contains(@id,'suspend')]").click()
         ui_utils(self.web_session).sleep(10)
         self.web_session.web_driver.find_element_by_xpath("//button[@alt='Suspend']").click()
@@ -119,22 +75,18 @@ class eap_operations():
         return True
 
     def resume_eap(self):
-        self.web_session.web_driver.get("{}/middleware_domain/show_list".format(self.web_session.MIQ_URL))
-        assert ui_utils(self.web_session).waitForTextOnPage("Domain Name ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'master')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("master (Summary)", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Server Groups')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage(" Server Group Name ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'main-server-group')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Properties ", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Servers')]")
-        assert ui_utils(self.web_session).waitForTextOnPage("Server Name", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'JBoss EAP')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Properties", 15)
-        self.web_session.web_driver.find_element_by_xpath("//button[@title='Power']").click()
+        self.eap_power()
         self.web_session.web_driver.find_element_by_xpath("//a[contains(@id,'resume')]").click()
         ui_utils(self.web_session).accept_alert(20)
+        ui_utils(self.web_session).sleep(10)
         assert ui_utils(self.web_session).waitForTextOnPage("Resume initiated for selected server(s)", 15)
+        return True
+
+    def start_eap(self):
+        self.eap_power()
+        self.web_session.web_driver.find_element_by_xpath("//a[contains(@title,'Start this server')]").click()
+        ui_utils(self.web_session).accept_alert(20)
+        assert ui_utils(self.web_session).waitForTextOnPage("Resume initiated for selected server(s)", 20)
         return True
 
 
