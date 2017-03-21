0918f08ff140fd99e951669d8d8d2708ca8558c9
Sunil Kondkar <skondkar@dhcp201-106.englab.pnq.redhat.com>
2017-03-21 17:38:40 +0530

    Changing image to cfme58

--- scripts/automation/docker/startCFME/setDefaultEnv.sh
+++ scripts/automation/docker/startCFME/setDefaultEnv.sh
@@ -2,10 +2,10 @@
 
 source ${DIR}/../common/common.sh
 
-export CFME_IMAGE="$DOCKER_HOST_BREW/cloudforms/cfme:latest"
+export CFME_IMAGE="$DOCKER_HOST_BREW/cloudforms/cfme58:latest"
 
 export CFME_PORT=80
 export CFME_SECURED_PORT=443
 export POSTGRESQL_PORT=5432
 
-export CFME_START_CMD="docker run --privileged -di -p $CFME_PORT:80 -p $CFME_SECURED_PORT:443 -p $POSTGRESQL_PORT:5432 $DOCKER_HOST_BREW/cloudforms/cfme:latest"
+export CFME_START_CMD="docker run --privileged -di -p $CFME_PORT:80 -p $CFME_SECURED_PORT:443 -p $POSTGRESQL_PORT:5432 $DOCKER_HOST_BREW/cloudforms/cfme58:latest"
