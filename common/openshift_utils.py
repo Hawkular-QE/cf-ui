8456c08e8b35aad27f1aae9369abc13c996dc8aa
mattmahoneyrh <mmahoney@redhat.com>
2017-07-10 15:54:07 -0400

    Openshift Utils

---
+++ common/openshift_utils.py
@@ -0,0 +1,29 @@
+from subprocess import Popen, PIPE, STDOUT
+import re
+
+class openshift_utils():
+    oc = 'tools/oc'
+    web_session = None
+    os_url = None
+
+    def __init__(self, web_session):
+        self.web_session = web_session
+        self.os_url = "https://{}:{}".format(web_session.OPENSHIFT_HOSTNAME, web_session.OPENSHIFT_PORT)
+        self.web_session.logger.info("Openshift URL: {}".format(self.os_url))
+
+        self.cli_login()
+
+    def __exec_cmd__(self, cmd):
+        return Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+
+    def cli_login(self):
+        p = self.__exec_cmd__("{} login {} -u 'user1' -p 'redhat'".format(self.oc, self.os_url))
+        output = p.stdout.read()
+        self.web_session.logger.debug("Openshift login: {}".format(output))
+
+    def get_token(self):
+        p = self.__exec_cmd__("{} whoami --token".format(self.oc))
+        token = re.sub(r"\W", "", p.stdout.read())
+        self.web_session.logger.info("Openshift Token: {}".format(token))
+
+        return token
