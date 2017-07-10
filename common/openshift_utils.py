d816e76dcae2ff55efdc47a81b503ee56e9ef419
mattmahoneyrh <mmahoney@redhat.com>
2017-07-10 16:14:39 -0400

    Parameterize creds

--- common/openshift_utils.py
+++ common/openshift_utils.py
@@ -10,16 +10,21 @@ class openshift_utils():
         self.web_session = web_session
         self.os_url = "https://{}:{}".format(web_session.OPENSHIFT_HOSTNAME, web_session.OPENSHIFT_PORT)
         self.web_session.logger.info("Openshift URL: {}".format(self.os_url))
-
         self.cli_login()
+
 
     def __exec_cmd__(self, cmd):
         return Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
 
+
     def cli_login(self):
-        p = self.__exec_cmd__("{} login {} -u 'user1' -p 'redhat'".format(self.oc, self.os_url))
+        p = self.__exec_cmd__("{} login {} -u {} -p {}"
+                .format(self.oc, self.os_url, self.web_session.OPENSHIFT_USERNAME, self.web_session.OPENSHIFT_PASSWORD))
         output = p.stdout.read()
-        self.web_session.logger.debug("Openshift login: {}".format(output))
+        self.web_session.logger.debug("Openshift login output: {}".format(output))
+        if (not "You have access" in output):
+            raise Exception("Login failed.")
+
 
     def get_token(self):
         p = self.__exec_cmd__("{} whoami --token".format(self.oc))
