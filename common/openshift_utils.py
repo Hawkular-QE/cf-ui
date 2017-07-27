36260453053c8efc91203f44b4b9bc466c09a84a
mattmahoneyrh <mmahoney@redhat.com>
2017-07-27 11:11:20 -0400

    Fixed get-token edge case

--- common/openshift_utils.py
+++ common/openshift_utils.py
@@ -1,42 +1,36 @@
-from subprocess import Popen, PIPE, STDOUT
-import re
+import subprocess
 
 class openshift_utils():
     oc = 'tools/oc'
     web_session = None
     os_url = None
+    login_cmd = None
 
     def __init__(self, web_session):
         self.web_session = web_session
         self.os_url = "https://{}:{}".format(web_session.OPENSHIFT_HOSTNAME, web_session.OPENSHIFT_PORT)
         self.web_session.logger.info("Openshift URL: {}".format(self.os_url))
-        self.cli_login()
+        self.login_cmd = "{} login {} -u {} -p {}".format(self.oc, self.os_url,
+                                self.web_session.OPENSHIFT_USERNAME, self.web_session.OPENSHIFT_PASSWORD)
 
 
     def __exec_cmd__(self, cmd):
         self.web_session.logger.info("OC cmd: {}".format(cmd))
-        return Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+        p = subprocess.Popen("/bin/bash", shell=True, stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p.stdin.write(cmd)
+        stdout, stderr = p.communicate()
 
+        self.web_session.logger.debug("STDOUT: {}".format(stdout))
+        self.web_session.logger.debug("STDERR: {}".format(stderr))
 
-    def cli_login(self):
-        p = self.__exec_cmd__("{} login {} -u {} -p {}"
-                .format(self.oc, self.os_url, self.web_session.OPENSHIFT_USERNAME, self.web_session.OPENSHIFT_PASSWORD))
-
-        stdout = p.stdout.read()
-        stderror = p.stderr.read()
-
-        if stderror:
-            raise Exception("Openshift command failed: {}".format(stderror))
-        else:
-            if not "Login successful" in stdout:
-                raise Exception("Openshift Login failed: {}".format(stdout))
-            else:
-                self.web_session.logger.debug("Openshift login output: {}".format(stdout))
-
+        return stdout, stderr
 
     def get_token(self):
-        p = self.__exec_cmd__("{} whoami --token".format(self.oc))
-        token = re.sub(r"\W", "", p.stdout.read())
+        cmd = "{};{} whoami --token".format(self.login_cmd, self.oc)
+        stdout, stderr = self.__exec_cmd__(cmd)
+        assert str(stdout) != '', "Error: No OC stdout text."
+        token = stdout.split()[-1]
         self.web_session.logger.info("Openshift Token: {}".format(token))
 
-        return token
+        return(token)
