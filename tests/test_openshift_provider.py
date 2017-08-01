1cfcb60ddc3ff75e518838fb401e3844298403ca
Prachi Yadav <pyadav@pyadav.pnq.csb>
2017-08-01 23:35:47 +0530

    crosslink test added

--- tests/test_openshift_provider.py
+++ tests/test_openshift_provider.py
@@ -1,6 +1,8 @@
 import pytest
 from common.session import session
 from views.mm_openshift_provider import mm_openshift_providers
+from views.providers import providers
+from views.eap_crosslink import eap_crosslink
 
 
 @pytest.fixture (scope='session')
@@ -13,3 +15,11 @@ def web_session(request):
 def test_cfui_add_provider(web_session):
     provs = mm_openshift_providers(web_session)
     provs.add_mm_openshift_provider()
+
+def test_add_middleware_provider(web_session):
+    provs = providers(web_session)
+    provs.add_provider()
+
+def test_eap_crosslink(web_session):
+    provs = eap_crosslink(web_session)
+    provs.crosslink_eap()
