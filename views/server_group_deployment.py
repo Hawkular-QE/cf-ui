1fe433aaa3e49d2830003ac59aff610d9c48bb4c
Sunil Kondkar <skondkar@dhcp201-106.englab.pnq.redhat.com>
2017-11-21 20:46:51 +0530

    Fixed domain Tests.

--- views/server_group_deployment.py
+++ views/server_group_deployment.py
@@ -11,6 +11,7 @@ from common.navigate import navigate
 from common.ui_utils import ui_utils
 from hawkular.hawkular_api import hawkular_api
 from common.db import db
+import pytest
 
 
 class server_group_deploymnt():
@@ -25,23 +26,29 @@ class server_group_deploymnt():
         self.web_session = web_session
         self.web_driver = web_session.web_driver
         self.hawkular_api = hawkular_api(self.web_session)
+        self.ui_utils = ui_utils(self.web_session)
 
     def add_deployment(self, app_to_deploy,runtime_name=None, enable_deploy=True, overwrite=False, cancel=False):
 
         app = "{}/data/{}".format(os.getcwd(), app_to_deploy)
 
         navigate(self.web_session).get("{}/middleware_domain/show_list".format(self.web_session.MIQ_URL))
-        assert ui_utils(self.web_session).waitForTextOnPage("Middleware Domains", 15)
-        self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Unnamed Domain')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Unnamed Domain (Summary)", 15)
+        assert ui_utils(self.web_session).waitForTextOnPage("master", 15)
+
+        domains_ui = self.ui_utils.get_list_table()
+        if not domains_ui:
+            self.web_session.logger.warning("No Domains found.")
+            pytest.skip("Skip test - No Domains found.")
+
+        self.ui_utils.click_on_row_containing_text(domains_ui[0].get('Domain Name'))
+        assert ui_utils(self.web_session).waitForTextOnPage("State", 15)
         self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'Middleware Server Groups')]").click()
-        assert ui_utils(self.web_session).waitForTextOnPage("Unnamed Domain (All Middleware Server Groups)", 15)
+        assert ui_utils(self.web_session).waitForTextOnPage("Server Group Name", 30)
         self.web_session.web_driver.find_element_by_xpath("//td[contains(.,'other-server-group')]").click()
         assert ui_utils(self.web_session).waitForTextOnPage("other-server-group (Summary)", 15)
+
         self.web_session.web_driver.find_element_by_xpath("//button[@title='Deployments']").click()
         self.web_session.web_driver.find_element_by_xpath("//a[@title='Add a new Deployment']").click()
-
-
 
         self.web_session.logger.info("Deploying App: {}".format(app))
         assert ui_utils(self.web_session).waitForTextOnPage("Select the file to deploy", 15)
